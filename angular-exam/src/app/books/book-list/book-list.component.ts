37850b4e4ab739193ce170861ac66e2c95cc9416
Tamas Vajgely <vajgi90@gmail.com>
2020-02-10 15:30:43 +0100

    test

---
+++ angular-exam/src/app/books/book-list/book-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book.model';
+import { Newspaper } from '../newspaper.model';
+import { BookService } from '../book.service';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+  private books = [
+    new Book('Stars from Eger', 'The revenge of Jumurdzsák'),
+    new Book('Boys from Pál street', 'RIP Nemecsek'),
+    new Book('Sons of a man with stone heart', '')
+  ];
+
+  private tempBooks: Book[];
+
+  private newspapers = [
+    new Newspaper('Blikk'),
+    new Newspaper('Népszabadság'),
+    new Newspaper('Magyar Nemzet'),
+  ];
+
+  constructor(private readonly bookService: BookService ) { }
+
+  ngOnInit() {
+    this.bookService.getBooks().subscribe(
+      data => {this.tempBooks = data}
+    )
+    this.tempBooks.map(x => {this.books.push(x)})
+  }
+
+}
---
+++ angular-exam/src/app/books/book/book.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Book } from '../book.model';
+import { LoggerService } from 'src/app/logger.service';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit {
+  @Input() model: Book;
+
+  constructor(private readonly loggerService: LoggerService) { }
+
+  ngOnInit() {
+    this.loggerService.log(this.model.title);
+  }
+}
